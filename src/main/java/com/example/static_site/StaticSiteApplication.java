f518dea1e91034e920eed53552b3ee78a29c0c8f
Thales Dev <thalesbarros7@hotmail.com>
2024-08-12 21:21:58 -0300

    teste-static

---
+++ src/main/java/com/example/static_site/StaticSiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.static_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaticSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaticSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/static_site/StaticSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.static_site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaticSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
